7244d8b24912161910cc73ac1eb1ca32438f956b
Rafael <rafael.lobao104@academico.ifs.edu.br>
2026-05-12 21:39:42 -0300

    Client Model

---
+++ src/main/java/com/barbershop/model/ClientModel.java
@@ -0,0 +1,16 @@
+package com.barbershop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity //Define que é uma entidade de uma tabela
+@Data //boilerplate para getters setters constructors
+@Table(name = "client")
+public class ClientModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
+}
